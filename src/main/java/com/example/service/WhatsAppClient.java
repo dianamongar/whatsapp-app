829763825d58710b3f29ca15c7995624ccc85a42
CarrascoAlejandro <carrascoalejandro2003@gmail.com>
2025-03-06 16:13:47 -0400

    ayudando a generar native

--- src/main/java/com/example/service/WhatsAppClient.java
+++ src/main/java/com/example/service/WhatsAppClient.java
@@ -1,16 +1,16 @@
-package com.example.service;
-
-import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
-import jakarta.ws.rs.*;
-import jakarta.ws.rs.core.MediaType;
-
-@RegisterRestClient(baseUri = "https://api.twilio.com/2010-04-01/Accounts/{AccountSid}/Messages.json")
-public interface WhatsAppClient {
-
-    @POST
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    @Produces(MediaType.APPLICATION_JSON)
-    String sendMessage(@FormParam("To") String to, 
-                       @FormParam("From") String from, 
-                       @FormParam("Body") String body);
-}
+//package com.example.service;
+//
+//import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+//import jakarta.ws.rs.*;
+//import jakarta.ws.rs.core.MediaType;
+//
+//@RegisterRestClient(baseUri = "https://api.twilio.com/2010-04-01/Accounts/{AccountSid}/Messages.json")
+//public interface WhatsAppClient {
+//
+//    @POST
+//    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+//    @Produces(MediaType.APPLICATION_JSON)
+//    String sendMessage(@FormParam("To") String to,
+//                       @FormParam("From") String from,
+//                       @FormParam("Body") String body);
+//}
