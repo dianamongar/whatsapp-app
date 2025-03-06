91e062e4a324efc00232d1f3d62a391ab61dee9f
CarrascoAlejandro <carrascoalejandro2003@gmail.com>
2025-03-06 17:29:51 -0400

    ir works now

--- src/main/java/com/example/dto/MessageReflectionConfiguration.java
+++ src/main/java/com/example/dto/MessageReflectionConfiguration.java
@@ -1,7 +1,19 @@
 package com.example.dto;
 
 import io.quarkus.runtime.annotations.RegisterForReflection;
+import com.twilio.converter.CurrencyDeserializer;
+import com.twilio.converter.DateConverter;
+import com.twilio.converter.Promoter;
+import com.twilio.converter.PrefixedCollapsibleMap;
+import com.twilio.type.PhoneNumber;
 
-@RegisterForReflection(targets = {com.twilio.rest.api.v2010.account.Message.class})
+@RegisterForReflection(targets = {
+        com.twilio.rest.api.v2010.account.Message.class,
+        PhoneNumber.class,
+        DateConverter.class,
+        CurrencyDeserializer.class,
+        Promoter.class,
+        PrefixedCollapsibleMap.class
+})
 public class MessageReflectionConfiguration {
 }
